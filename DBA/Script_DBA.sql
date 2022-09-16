d3dbb679f39bd10a1ac7d3ab5ac27e6a5338c5d5
Omar David Vega <odvega.co@gmail.com>
2022-09-16 18:52:38 -0500

    Se ingresa el scritp DBA

---
+++ DBA/Script_DBA.sql
@@ -0,0 +1,23 @@
+create table car (
+  ID   number,
+  BRAND varchar2(20),
+  MODEL   number,
+  CATEGORY_ID   number,
+  primary key(ID) 
+);
+
+create table CLIENT (ID NUMBER,
+NAME VARCHAR(4000),
+EMAIL VARCHAR2(20),
+AGE NUMBER,
+primary key(ID) 
+
+ ); 
+
+ create table MESSAGE (
+ID NUMBER,
+MESSAGETEXT VARCHAR2(4000),
+primary key(ID) 
+ );
+ 
+ 
