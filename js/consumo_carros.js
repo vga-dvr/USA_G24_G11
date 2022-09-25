d5e9c3f23dc50c1a76079a1220633ba70b795ae1
Omar David Vega <odvega.co@gmail.com>
2022-09-24 22:48:58 -0500

    V_2

--- js/consumo_carros.js
+++ js/consumo_carros.js
@@ -1,5 +1,4 @@
 //Declaración de varibles base de datos Oracle
-
 UrlCar = "https://gb47cdc0cd0d1af-productos.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/car/car"
 
 
@@ -39,11 +38,6 @@ function peticionGet() {
     });
 }
 //Peticion Post modulo de CRUD carros
-
-UrlCar= "https://gb47cdc0cd0d1af-productos.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/car/car"
-UrlPut= "https://g4cab850130b15d-ciclo3proyectocar.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/car/car"
-
-
 function peticionPost() {
     var diccionario = {
         id: $("#id").val(),
@@ -63,20 +57,14 @@ function peticionPost() {
     });
 
 }
-
 //Peticion Put modulo de CRUD carros
 function peticionPut() {
-
-
-function peticionPut(){
-
     var diccionarioPut = {
         id: $("#idPut").val(),
         brand: $("#brandPut").val(),
         model: $("#modelPut").val(),
         category_id: $("#category_idPut").val()
     };
-
 
     var jsonPut = JSON.stringify(diccionarioPut);
     $.ajax({
@@ -86,24 +74,12 @@ function peticionPut(){
         contentType: "application/json",
         complete: function (response) {
             if (response.status == 201) {
-
-    
-    var jsonPut = JSON.stringify(diccionarioPut);
-    $.ajax({
-        type:'PUT',
-        url:UrlCar,
-        data: jsonPut,
-        contentType:"application/json",
-        complete:function(response){
-            if(response.status == 201){
-
                 alert("Editado con éxito!!")
             }
             console.log(response.status)
         }
     });
     console.log(jsonPut)
-
 
 }
 
@@ -120,24 +96,6 @@ function peticionDelete() {
         contentType: "application/json",
         complete: function (response) {
             if (response.status == 204) {
-
-    
-}
-
-function peticionDelete(){
-    var diccionarioDelete = {
-        id: $("#idDelete").val(),
-    };
-    
-    var jsonDelete = JSON.stringify(diccionarioDelete);
-    $.ajax({
-        type:'DELETE',
-        url:UrlCar,
-        data: jsonDelete,
-        contentType:"application/json",
-        complete:function(response){
-            if(response.status == 204){
-
                 alert("Eliminado con éxito!!")
             }
             console.log(response.status)
